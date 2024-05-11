2e35a249ca565931f2c144faf8d569288607e9f7
alansanchezzamora <122119972+alansanchezzamora@users.noreply.github.com>
2024-05-11 00:13:30 -0600

    correct

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import productDetails from "./ProductDetails.mjs";
 
@@ -8,6 +8,10 @@ const dataSource = new ProductData("tents");
 const productDetail = new productDetails(productId, dataSource);
 productDetail.init();
 // Add to cart button event handler
+
+/* function addProductToCart(product) {
+  setLocalStorage("so-cart", product);
+} */
 
 async function addToCartHandler(e) {
   try {
